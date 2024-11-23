14f0ffacf61482cb45c0601b34bb756916a70000
Kirill Valikov <Kirill.Valikov@infowatch.com>
2024-11-23 20:05:21 +0300

    init website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import RatingWidget from './rating-widget'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='header'>Thank you for using <img src="./28x28.png" />Edit Website Extension!</div>
+      <RatingWidget></RatingWidget>
+    </>
+  )
+}
+
+export default App
