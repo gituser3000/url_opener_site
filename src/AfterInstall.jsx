63af16dc056b7ff29c0d29a165d4d4573a605f54
Kirill Valikov <Kirill.Valikov@infowatch.com>
2025-03-04 22:58:49 +0300

    fix texts

--- src/AfterInstall.jsx
+++ src/AfterInstall.jsx
@@ -3,11 +3,11 @@ import './AfterInstall.css'
 function AfterInstall() {
   return (
     <>
-      <h2 className="header">Chrome Reader Mode installed!</h2>
+      <h2 className="header">Url Opener installed!</h2>
       <div className='descr'>Click the puzzle piece (1) in the top right of your browser.</div>
       <div className='descr'>Then, click the little pin (2) next to the extension:</div>
       <img className='img' src="./pin.png" alt="" />
-      <div className='descr'>Activate the extension (3) on any <a href="https://www.lipsum.com/" target="_blank">webpage with article</a> you want to read:</div>
+      <div className='descr'>Activate the extension (3) to open links:</div>
       <img className='img' src="./run.png" alt="" />
     </>
   )
